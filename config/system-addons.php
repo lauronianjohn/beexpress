8c485f90585b69593cd71d730ddfc9cbef968cb5
Ian John <lauronianjohn101@gmail.com>
2025-08-29 17:27:50 +0800

    init

--- config/system-addons.php
+++ config/system-addons.php
@@ -1,14 +1,14 @@
 <?php return array (
-  'admin_panel' =>
+  'admin_panel' => 
   array (
-    'active' => '0',
-    'username' => '',
-    'purchase_key' => '',
-    'software_id' => '',
-    'domain' => '',
+    'active' => '1',
+    'username' => 'sixamtech',
+    'purchase_key' => '51887f83-464d-4364-ba7e-1f130d48f82a',
+    'software_id' => 'MzM1NzE3NTA=',
+    'domain' => 'localhost:8000',
     'software_type' => 'product',
   ),
-  'vendor_panel' =>
+  'vendor_panel' => 
   array (
     'active' => '0',
     'username' => '',
@@ -17,7 +17,7 @@
     'domain' => '',
     'software_type' => 'addon',
   ),
-  'user_app' =>
+  'user_app' => 
   array (
     'active' => '0',
     'username' => '',
@@ -26,7 +26,7 @@
     'domain' => '',
     'software_type' => 'addon',
   ),
-  'restaurant_app' =>
+  'restaurant_app' => 
   array (
     'active' => '0',
     'username' => '',
@@ -35,7 +35,7 @@
     'domain' => '',
     'software_type' => 'addon',
   ),
-  'deliveryman_app' =>
+  'deliveryman_app' => 
   array (
     'active' => '0',
     'username' => '',
@@ -44,7 +44,7 @@
     'domain' => '',
     'software_type' => 'addon',
   ),
-  'react_web' =>
+  'react_web' => 
   array (
     'active' => '0',
     'username' => '',
